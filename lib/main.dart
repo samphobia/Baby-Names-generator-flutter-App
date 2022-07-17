dd280f5f87297c2dbafc3e80cdb7e5e2c551cd19
samphobia <samobafemisayo@gmail.com>
2022-07-17 14:21:23 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:english_words/english_words.dart';
+import 'package:username/username.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Baby Names recommender',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Baby Names'),
+        ),
+        body: const Center(
+          child: RandomWords(),
+        ),
+      ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _biggerFont = const TextStyle(fontSize: 18);
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        return ListTile(title: Text(Username.en().fullname,
+            style: _biggerFont));
+      },
+    );
+  }
+}
